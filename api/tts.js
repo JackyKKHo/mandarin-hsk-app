cddf876a6cf21b825b4c6ad561f03aac4e1ad4ff
Jacky <jacky6556@gmail.com>
2026-05-03 16:03:49 +0100

    Fix Chinese TTS voice name

--- api/tts.js
+++ api/tts.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         input: { text },
         voice: {
           languageCode: 'cmn-CN',
-          name: 'cmn-CN-Neural2-D',
+          name: 'cmn-CN-Wavenet-D',
           ssmlGender: 'FEMALE',
         },
         audioConfig: {
